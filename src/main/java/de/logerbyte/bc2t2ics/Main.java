e1dc5b5351488652a43d70c61221ddfad73e8f28
wolfram <wolfram@adesso-mobile.de>
2017-12-18 18:39:12 +0100

    create controller

---
+++ src/main/java/de/logerbyte/bc2t2ics/Main.java
@@ -0,0 +1,7 @@
+package de.logerbyte.bc2t2ics;
+
+public class Main {
+    public static void main(String[] args) {
+        new MainController(args);
+    }
+}
---
+++ src/main/java/de/logerbyte/bc2t2ics/MainController.java
@@ -0,0 +1,16 @@
+package de.logerbyte.bc2t2ics;
+
+import de.logerbyte.bc2t2ics.bc2t.Bc2TaskJson;
+import de.logerbyte.bc2t2ics.bc2t.Bc2TaskReader;
+
+import java.io.FileNotFoundException;
+
+class MainController {
+    MainController(String[] args) {
+        try {
+            Bc2TaskJson[] bc2tJsonFromFile = new Bc2TaskReader().createBc2tJsonFromFile(args[0]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
