84250479af2eb61e107aa2e769070393e3495fe0
Stefan_Wolfram <logerom@gmx.de>
2018-01-20 00:16:57 +0100

    add separator for path name

--- src/test/java/de/logerbyte/bc2t2ics/MainControllerTest.java
+++ src/test/java/de/logerbyte/bc2t2ics/MainControllerTest.java
@@ -3,6 +3,8 @@ package de.logerbyte.bc2t2ics;
 import de.logerbyte.bc2t2ics.bc2t.Bc2TaskReader;
 import org.junit.Test;
 
+import java.io.File;
+
 public class MainControllerTest {
 
     public MainControllerTest() {
@@ -10,7 +12,7 @@ public class MainControllerTest {
 
     @Test
     public void testCreateJson() throws Exception {
-        new Bc2TaskReader().createBc2tJsonFromFile("bc2t_test_file\\bc2t_test.txt");
+        new Bc2TaskReader().createBc2tJsonFromFile("bc2t_test_file"+ File.separator+"bc2t_test.txt");
     }
 
 }
